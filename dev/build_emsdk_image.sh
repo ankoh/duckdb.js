b1e0123da59adaef3bc3ac74c3fd8c19249779f3
Andre Kohn <kohn.a@outlook.com>
2020-09-29 00:02:22 +0200

    Tweak emsdk image

--- dev/build_emsdk_image.sh
+++ dev/build_emsdk_image.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 PROJECT_ROOT="$(cd $(dirname "$BASH_SOURCE[0]") && cd .. && pwd)" &> /dev/null
 
 EMSDK_VERSION="2.0.4"
-DOCKER_IMAGE_NAMESPACE="ankoh"
+DOCKER_IMAGE_NAMESPACE="dashql"
 DOCKER_IMAGE_NAME="emsdk"
 DOCKER_IMAGE_TAG="${EMSDK_VERSION}"
 
@@ -14,6 +14,6 @@ cd ${PROJECT_ROOT} && \
     tar -cvf - ./dev/Dockerfile.emsdk | docker build \
         -t ${DOCKER_IMAGE_NAMESPACE}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} \
         -f ./dev/Dockerfile.emsdk \
-        --build-arg EMSDK_COMMIT="${EMSDK_VERSION}" \
+        --build-arg EMSDK_VERSION="${EMSDK_VERSION}" \
         -
 
